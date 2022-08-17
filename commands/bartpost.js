6727fb31a7c33650ffbe6d1ff75232e71819bccc
TannerTX <tanner_bibb@yahoo.com>
2022-08-16 22:27:36 -0500

    Added Admin verification to channel changer

---
+++ commands/bartpost.js
@@ -0,0 +1,11 @@
+const client = require("../index.js")
+
+module.exports = {
+    name: 'bartpost',
+    description: 'Bartposts xD',
+    usage: ',bartpost',
+    execute(message) {
+        message.channel.send("https://imgur.com/vt0KocM \n https://imgur.com/57p3jZ9 \n https://imgur.com/ZdaF1Fs \n https://cdn.discordapp.com/attachments/320432735871172608/691168686807056424/bart_4.mp4").then(msg => msg.delete({timeout: 10000}))
+    }
+
+}
--- commands/change.js
+++ commands/change.js
@@ -29,7 +29,28 @@ module.exports = {
                                 'LurkTest',
                                 'WhoTest']
             break;
+            
+            case 'starwars':
+                TEXT_CHANGE = ['𝐌𝐨𝐬 𝐄𝐢𝐬𝐥𝐞𝐲 𝐒𝐩𝐚𝐜𝐞𝐩𝐨𝐫𝐭',
+                               '𝐓𝐚𝐭𝐨𝐨𝐢𝐧𝐞 𝐁𝐚𝐝𝐥𝐚𝐧𝐝𝐬',
+                               '𝐉𝐚𝐰𝐚 𝐒𝐚𝐧𝐝𝐜𝐫𝐚𝐰𝐥𝐞𝐫']  
+                
+                VOICE_CHANGE = ['𝐌𝐨𝐬 𝐄𝐢𝐬𝐥𝐞𝐲 𝐂𝐚𝐧𝐭𝐢𝐧𝐚',
+                                '𝐒𝐭𝐨𝐫𝐦𝐭𝐫𝐨𝐨𝐩𝐞𝐫 𝐉𝐚𝐜𝐮𝐳𝐳𝐢',
+                                '𝐃𝐞𝐚𝐭𝐡 𝐒𝐭𝐚𝐫 𝐓𝐫𝐞𝐧𝐜𝐡',
+                                '𝐁𝐨𝐮𝐧𝐭𝐲 𝐇𝐮𝐧𝐭𝐞𝐫𝐬 𝐎𝐧𝐥𝐲',
+                                '𝐌𝐢𝐧𝐢𝐤𝐢𝐭 𝐇𝐮𝐧𝐭𝐢𝐧𝐠']
+
+                ROLES_CHANGE = ['𝐒𝐢𝐭𝐡 𝐑𝐨𝐲𝐚𝐥 𝐆𝐮𝐚𝐫𝐝𝐬',
+                                '𝐆𝐫𝐚𝐧𝐝 𝐀𝐝𝐦𝐢𝐫𝐚𝐥𝐬',
+                                '𝐒𝐭𝐨𝐫𝐦𝐭𝐫𝐨𝐨𝐩𝐞𝐫𝐬',
+                                '𝐉𝐚𝐰𝐚𝐬',
+                                '𝐔𝐠𝐧𝐚𝐮𝐠𝐡𝐭𝐬']
+            break;
         }
+
+        /*   Execute Change   */
+        if(message.member.roles.cache.has(client.OWNER_ROLE)) {
 
         for(const [INDEX, ELEMENT] of client.TEXT_CHANNELS.entries())
             client.CHANNELS.cache.get(ELEMENT).setName(TEXT_CHANGE[INDEX])
@@ -37,9 +58,10 @@ module.exports = {
         for(const [INDEX, ELEMENT] of client.VOICE_CHANNELS.entries())
             client.CHANNELS.cache.get(ELEMENT).setName(VOICE_CHANGE[INDEX])
         
-        for(const [INDEX, ELEMENT] of client.ROLE_CHANNELS.entries())
+        for(const [INDEX, ELEMENT] of client.ROLES.entries())
             client.CHANNELS.cache.get(ELEMENT).setName(ROLES_CHANGE[INDEX])
-
+        }
+        else message.channel.send(`${message.author} | Insufficient Permissions!`)
 
     }
 
