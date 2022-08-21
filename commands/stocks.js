d243f2c204afa19d041b0086644479911784e762
TannerTX <tanner_bibb@yahoo.com>
2022-08-21 04:14:52 -0500

    Added ability to list active symbols in stocks DB

--- commands/stocks.js
+++ commands/stocks.js
@@ -1,5 +1,6 @@
 const client = require("../index.js")
 const yahooStockPrices = require('yahoo-stock-prices')
+const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
     name: 'stocks',
@@ -17,6 +18,7 @@ module.exports = {
         const check_if_valid_symbol = async (symbol) => {
             try {
                 const data = await yahooStockPrices.getCurrentData(symbol)
+                console.log(data)
                 return true
             }
             catch (e) { return false }
@@ -51,6 +53,27 @@ module.exports = {
                         client.symbolModel.deleteOne({ symbol: sym }).then(res => message.channel.send(`Successfully removed \`${sym}\` from the database!`).then(msg => setTimeout(() => msg.delete(), 5000)))
                     else
                         message.channel.send(`${message.author} | Symbol does not exist in the database!`).then(msg => setTimeout(() => msg.delete(), 5000))
+                    break
+
+                case 'list':
+                    client.symbolModel.find({}).then(res => {
+                        if(!res)
+                        message.channel.send(`Query returned NULL | Database is empty!`).then(msg => setTimeout(() => msg.delete(), 5000))
+                        else {
+
+                            const STOCK_EMBED = new EmbedBuilder()
+                                .setColor("#1bf2c0")
+                                .setTitle("Active Stock Symbols")
+
+                            var POS = 1
+                            res.forEach(stock => {
+                                 STOCK_EMBED.addFields({name:`**${POS}**. \`${stock.symbol}\``, value:`------------`})
+                                 POS++
+                            })
+                            message.channel.send({ embeds: [STOCK_EMBED] }).then(msg => setTimeout(() => msg.delete(), 25000))
+                        }
+                        
+                    })
                     break
             }
         }
